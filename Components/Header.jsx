5c5fc179c76de5081caa9de34c37dd733c995dc8
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-03-19 17:32:19 +0530

    Done changes in header section

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -2,37 +2,6 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 const Header = () => {
-  //   const [showContactDetails, setShowContactDetails] = useState(true);
-  //   const [lastScrollY, setLastScrollY] = useState(0);
-
-  //   useEffect(() => {
-  //     const handleScroll = () => {
-  //       const currentScrollY = window.scrollY;
-
-  //       // Show shadow if scrolled down
-  //       setShowShadow(currentScrollY > 0);
-
-  //       // Show/hide contact details based on scroll direction
-  //       if (currentScrollY > lastScrollY) {
-  //         // Scrolling down
-  //         setShowContactDetails(false);
-  //       } else {
-  //         // Scrolling up
-  //         setShowContactDetails(true);
-  //       }
-
-  //       // Update last scroll position
-  //       setLastScrollY(currentScrollY);
-  //     };
-
-  //     // Attach scroll event listener
-  //     window.addEventListener("scroll", handleScroll);
-
-  //     // Cleanup event listener on component unmount
-  //     return () => {
-  //       window.removeEventListener("scroll", handleScroll);
-  //     };
-  //   }, [lastScrollY]);
 
   return (
     <div className="w-full bg-[#00283C] flex justify-around items-center gap-10 drop-shadow-xl">
@@ -52,11 +21,6 @@ const Header = () => {
           {/* <p>English</p> */}
           <p>7897897890</p>
         </div>
-        {/* )} */}
-        {/* <div className="flex gap-10">
-               <button className="text-white px-10 py-5 rounded-full font-bold hover:bg-white hover:text-black">Find a Job</button>
-               <button className="bg-yellow-400 px-10 py-5 rounded-full font-bold hover:bg-white">Hire Talent</button>
-           </div> */}
       </div>
     </div>
   );
